6355be4356b58d6ce909d5ed0ff5804d6a10bc55
Burich1985 <ub@e-cats.ru>
2020-03-27 09:16:25 +0300

    top menu complete

--- src/components/top-menu/top-menu.jsx
+++ src/components/top-menu/top-menu.jsx
@@ -7,18 +7,18 @@ import './top-menu.scss'
 const menuTree = [
   {label: 'Генеральный план', to: '/master-plan/'},
   {label: 'Настройки', submenu: [
-    {label: 'Конструктивные элементы (КЭ)', to: 'structural-elements/'},
-    {label: 'Справочники норм и расценок (УЕР)', to: 'references-and-rates'},
-    {label: 'Виды работ СМР', to: 'types-of-jobs'},
-    {label: 'Проектные марки', to: 'design-brands'},
+    {label: 'Конструктивные элементы (КЭ)', to: '/structural-elements/'},
+    {label: 'Справочники норм и расценок (УЕР)', to: '/references-and-rates/'},
+    {label: 'Виды работ СМР', to: '/types-of-jobs/'},
+    {label: 'Проектные марки', to: '/design-brands/'},
     {label: 'Ед.изм', to: '/measure-units/'},
   ]},
   {label: 'Рабочее место методолога', submenu: [
-    {label: 'Правила связи УЕР и КЭ', to: 'communication-rules-uer'},
-    {label: 'Правила связи Видов работ и КЭ', to: 'communication-rules-types-of-jobs'},
+    {label: 'Правила связи УЕР и КЭ', to: '/communication-rules-uer/'},
+    {label: 'Правила связи Видов работ и КЭ', to: '/communication-rules-types-of-jobs/'},
   ]},
-  {label: 'Ведомость объемов работ', to: 'bill-of-quantities'},
-  {label: 'Технологическая последовательность работ', to: 'workflow'},
+  {label: 'Ведомость объемов работ', to: '/bill-of-quantities/'},
+  {label: 'Технологическая последовательность работ', to: '/workflow/'},
 ]
 
 class TopMenu extends Component {
@@ -32,6 +32,24 @@ class TopMenu extends Component {
         hoveredIdx: hoveredIdx === idx ? undefined : hoveredIdx
       }
     });
+  }
+
+  createSubmenu = (submenu) => {
+    return (
+    <div className="sub-menu">
+    {
+      submenu.map(({label, to}, sub_idx) => {
+        return (
+          <Link to={to} key={sub_idx}>
+            <div className="sub-menu-item">
+              {label}
+            </div>
+          </Link>
+        )
+      })
+    }
+    </div>
+    )
   }
 
   render() {
@@ -66,23 +84,8 @@ class TopMenu extends Component {
                 >
                   <span>{label}</span>
                   {
-                    ((this.state.hoveredIdx === idx) && submenu && submenu.length) ?
-                      (
-                        <div className="sub-menu">
-                        {
-                          submenu.map(({label, to}, sub_idx) => {
-                            return (
-                              <Link to={to} key={sub_idx}>
-                                <div className="sub-menu-item">
-                                  {label}
-                                </div>
-                              </Link>
-                            )
-                          })
-                        }
-                        </div>
-                      ) 
-                      : null
+                    ((this.state.hoveredIdx === idx) && submenu && submenu.length) 
+                    ? this.createSubmenu(submenu) : null
                   }
                 </div>
               )
