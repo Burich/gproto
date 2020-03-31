a6abf4f3a6ff5a464df6e60771bafa2146fa71ed
Burich1985 <ub@e-cats.ru>
2020-03-31 10:05:49 +0300

    main link

--- src/components/main-header/main-header.jsx
+++ src/components/main-header/main-header.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import { Link } from "react-router-dom";
 
+import { config } from "../../constants";
+
 import './main-header.scss'
 
 const MainHeader = () => {
   return (
     <div className="main-header">
-      <Link to='/'>
+      <Link to={config.link('/')}>
         Интеграционное решение
       </Link>
     </div>
