00130ac641d7f56634e2a722923c1277f42d48dd
Burich1985 <ub@e-cats.ru>
2020-03-31 09:39:32 +0300

    Updates

---
+++ precache-manifest.a64df5f0e1fb4fb701b94f34693b47d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b3b18d5af4478b32809113e402d6ee",
+    "url": "/gproto/index.html"
+  },
+  {
+    "revision": "1db8b76d95f4a0cbf7d9",
+    "url": "/gproto/static/css/main.cc23bdbf.chunk.css"
+  },
+  {
+    "revision": "2b50b2687cfff2f31e4d",
+    "url": "/gproto/static/js/2.be3945f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gproto/static/js/2.be3945f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db8b76d95f4a0cbf7d9",
+    "url": "/gproto/static/js/main.fc14da2d.chunk.js"
+  },
+  {
+    "revision": "c27451cab4861586be1a",
+    "url": "/gproto/static/js/runtime-main.f18dd4b9.js"
+  }
+]);
